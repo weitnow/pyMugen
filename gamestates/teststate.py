2ff962b6a9856c78cdd8f44a50db4d96582abc4b
weitnow <weitnow@gmail.com>
2026-04-13 21:19:37 +0200

    ref

--- gamestates/teststate.py
+++ gamestates/teststate.py
@@ -1,3 +1,6 @@
+import random
+import pygame
+
 from gamestates.gamestate_base import GameState
 from gameobjects.game_object import GameObject, HitboxType, HurtboxType
 from input_manager import PlayerController, Action
@@ -11,7 +14,7 @@ class TestState(GameState):
     def enter(self):
         #create a game object and add sprite
         self.mySprite1 = Sprite().set_anim_name("debug32")
-       
+        self.particles = []
 
 
        
@@ -30,14 +33,30 @@ class TestState(GameState):
 
     def update(self, dt):
         #self.view_manager.camera.update(self.player1, self.player2) # simple camera follow for testing, can be expanded later for more complex behavior (like lookahead, shake, etc)
+        
+        self.particles.append([[100, 150], [random.randint(0, 20) / 10 - 1, -2], random.randint(1,3)])
+        
+        for particle in self.particles:
+            particle[0][0] += particle[1][0]
+            particle[0][1] += particle[1][1]
+            particle[2] -= 0.1
+            particle[1][1] += 0.2
+            
+            if particle[2] <= 0:
+                self.particles.remove(particle)
+
         super().update(dt)
 
-        
+    
 
 
 
     def draw(self):
         self.mySprite1.draw(self.view_manager.game_surface, (32, 32))
+
+        for particle in self.particles:
+            pygame.draw.circle(self.view_manager.game_surface, (230, 5, 76), [int(particle[0][0]), int(particle[0][1])], int(particle[2]))
+
         super().draw()
 
 
